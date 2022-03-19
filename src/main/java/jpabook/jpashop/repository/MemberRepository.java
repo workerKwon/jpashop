1309506b98b38abf2bba83e0c2b5f13e6c965f31
workerKwon <dmdmdm1322@gmail.com>
2022-03-19 12:10:56 +0900

    implement member repository

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import jpabook.jpashop.domain.Member;
+
+@Repository
+public class MemberRepository {
+    
+    @PersistenceContext // 스프링이 생성한 entityManager를 주입해주는 어노테이션
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
