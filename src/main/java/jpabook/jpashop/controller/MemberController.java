7321bdd6ca4d9bc356991611d34fbcfe402ceaef
workerKwon <dmdmdm1322@gmail.com>
2022-03-26 15:54:56 +0900

    회원 등록 컨트롤러 개발

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    /**
+     * MemberForm을 사용하는 이유
+     * 컨트롤러에 넘어올 때의 데이터와 엔티티에서 원하는 데이터가 다르기 때문에
+     * 필요한 데이터만 담아내는 객체를 만들어서 사용
+     */
+    @PostMapping("members/new")
+    public String create(@Valid MemberForm memberForm, BindingResult result) {
+
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
